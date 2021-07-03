8e2232b55acca5d0ea0e55566dfe6d4d58880e7e
itta611 <86896657+itta611@users.noreply.github.com>
2021-07-04 08:05:00 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+"use strict";
+let projectId;
+let zip = new JSZip();;
+$('#load-btn').click(function() {
+  projectId = $('#project-id').val();
+  $('#load-btn').text('LOADING...');
+  $.ajax({
+    url: `https://projects.scratch.mit.edu/${projectId}/`,
+    data: "",
+    dataType: "text",
+    success: function (response) {
+      $('#json-editor').val(response);
+      $('#json-editor').removeAttr('disabled');
+      $('#load-btn').text('LOAD');
+      $('#thumbnail').attr('src', `https://uploads.scratch.mit.edu/projects/thumbnails/${projectId}.png`);
+    },
+    error: function () {
+      alert('エラーが発生しました。\nThe error occurred.')
+      $('#load-btn').text('LOAD');
+    }
+  });
+});
+
+$('#export').click(function () {
+  let folder = zip.folder("project");
+  folder.file('project.json', $('#json-editor').val());
+  zip.generateAsync({type:"blob"})
+  .then(function(content) {
+      // see FileSaver.js
+      saveAs(content, "project.sb3");
+  });
+});
